ae468c9409bec5eb3740c72439cb00de771cce91
Adhco0543 <Adh@wbhb666.com>
2026-07-10 16:45:27 -0400

    Complete homepage section flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 import { Hero } from "@/components/Hero";
 import { Services } from "@/components/Services";
 import { Owners } from "@/components/Owners";
+import {
+  FAQPreview,
+  FinalCTA,
+  GalleryPreview,
+  Process,
+  WhyChooseUs,
+} from "@/components/HomeSections";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Services />
+      <WhyChooseUs />
+      <Process />
       <Owners />
+      <GalleryPreview />
+      <FAQPreview />
+      <FinalCTA />
     </>
   );
 }
