4024ee4221fc1f810a0bb076aa3caf0306729586
Adhco0543 <Adh@wbhb666.com>
2026-07-26 12:36:01 -0400

    Render local business schema sitewide

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { LocalBusinessSchema } from "@/components/LocalBusinessSchema";
 import { siteConfig } from "@/lib/config";
 
 const geistSans = Geist({
@@ -70,6 +71,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col bg-white text-slate-950">
+        <LocalBusinessSchema />
         <Header />
         <div className="flex-1">{children}</div>
         <Footer />
