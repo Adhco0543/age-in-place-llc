dc0cdec037b0a743d04bb06115a74d6d329e7a01
Adhco0543 <Adh@wbhb666.com>
2026-08-28 09:44:44 -0400

    Use canonical www URLs in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const baseUrl = "https://ageinplacellc.com";
+const baseUrl = "https://www.ageinplacellc.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const pages: Array<{
