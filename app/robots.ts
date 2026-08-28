94c7592071e4fe96df325b852d8036fddde77f0c
Adhco0543 <Adh@wbhb666.com>
2026-08-28 09:44:35 -0400

    Point robots sitemap to canonical www domain

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://ageinplacellc.com/sitemap.xml",
+    sitemap: "https://www.ageinplacellc.com/sitemap.xml",
   };
 }
